95e5863c783474402530c37fd70b9083ee467ac9
Délita <delita.makanda@axivit.com>
2026-04-17 11:40:45 +0200

    Refactor PracticalInfoResolver logic, improve SEO handling, and enhance dynamic page rendering with slug-based routes and fallback logic

--- src/app/features/practical-info/resolvers/practical-info.resolver.ts
+++ src/app/features/practical-info/resolvers/practical-info.resolver.ts
@@ -4,16 +4,18 @@ import { PracticalInfoApi } from '../services/practical-info.api';
 import { catchError, Observable, of } from 'rxjs';
 import { Page } from '../../../core/models/page.model';
 
-export const practicalInfoResolver: ResolveFn<Observable<Page | null>> = (route) => {
+export const practicalInfoResolver: ResolveFn<Page | null> = (route) => {
   const api = inject(PracticalInfoApi);
   const router = inject(Router);
 
-  const slug = route.url.map(urlSegment => urlSegment.path).join('/');
-  console.log('practicalInfoResolver slug:', slug);
+  const slug = route.paramMap.get('slug') ?? '';
+
   return api.getPageBySlug(slug).pipe(
-    catchError(()=> {
-      router.navigate(['/not-found']);
-      return of(null)
+    catchError((error) => {
+      router.navigateByUrl('/not-found');
+      return of(null);
     })
-  );
+  )
+
+
 };
