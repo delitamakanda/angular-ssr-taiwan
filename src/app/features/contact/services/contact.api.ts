32b027858e76fc3c0db61c13653f7b7ece7102a1
Délita <delita.makanda@axivit.com>
2026-04-17 16:05:30 +0200

    Inject API_CONFIG_TOKEN into ContactApi and update API URL usage for flexibility

--- src/app/features/contact/services/contact.api.ts
+++ src/app/features/contact/services/contact.api.ts
@@ -2,19 +2,20 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ContactForm, ContactResponse } from '../models/contact.model';
 import { Observable } from 'rxjs';
+import { API_CONFIG_TOKEN } from '../../../core/config/injection-token';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContactApi {
   private readonly http = inject(HttpClient);
-  private readonly apiUrl = '/api/contacts';
+  private readonly config = inject(API_CONFIG_TOKEN);
 
   sendContact(payload: ContactForm): Observable<ContactResponse> {
     const body: ContactForm = {
       ...payload,
       timestamp: Date.now(),
-    }
-    return this.http.post<ContactResponse>(this.apiUrl, body);
+    };
+    return this.http.post<ContactResponse>(this.config.wordpressApiBaseUrl, body);
   }
 }
