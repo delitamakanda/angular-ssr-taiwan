0b2e5f19518762a7f60c0d10aeff487ab0f8eab7
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-02 17:57:18 +0200

    Add loader component with animation for loading state in app shell

--- src/app/core/layout/shell/app-shell.component.ts
+++ src/app/core/layout/shell/app-shell.component.ts
@@ -2,18 +2,19 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer-component';
 import { HeaderComponent } from '../header/header-component';
-import { LoadingService } from '../../services/loading.service';
+import { LoadingService } from '@app/core/services/loading.service';
+import { LoaderComponent } from "@app/shared/component/loader/loader.component";
 
 @Component({
   selector: 'app-shell',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, LoaderComponent],
   standalone: true,
   template: `
     <div class="flex flex-col min-h-screen">
     <app-header />
       @if (loadingService.loading()) {
         <div class="flex items-center justify-center h-screen">
-          Loading...
+          <app-loader />
           </div>
       }
   <main class="flex-1 max-w-6xl mx-auto w-full px-4 py-6">
---
+++ src/app/shared/component/loader/loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  imports: [],
+  standalone: true,
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss'],
+})
+export class LoaderComponent {}
