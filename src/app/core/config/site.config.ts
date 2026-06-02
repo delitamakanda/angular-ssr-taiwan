785d77f84e72c01f2a25522c1d0c72de61ae4151
Délita MAKANDA <delita.makanda@axivit.com>
2026-06-02 17:50:10 +0200

    Update site URL in configuration to point to the deployed application

--- src/app/core/config/site.config.ts
+++ src/app/core/config/site.config.ts
@@ -1,4 +1,4 @@
 export const SITE_CONFIG = {
   site_name: 'Taiwan Travel Guide',
-  site_url: 'https://taiwan-travel-guide.com',
+  site_url: 'https://angular-ssr-taiwan-946790860424.europe-west9.run.app',
 }
